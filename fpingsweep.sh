45d6d668149b4146aab9f1f1665c4eb077b1e8df
cl4irv0yance <63370892+cl4irv0yance@users.noreply.github.com>
2022-09-26 09:31:13 -0500

    Create fpingsweep.sh

---
+++ fpingsweep.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$1" == "" ]
+then
+echo "Please enter the ip in the following format: 192.168.1"
+
+else
+
+for subnet; do
+ fping -g -r 1 $1.0/24 2>/dev/null | tee "$1"subnet.txt 
+done
+fi
